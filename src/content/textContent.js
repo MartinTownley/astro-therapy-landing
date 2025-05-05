e61a6dbe4c146916627c6b05b2e019098f0136f2
Merv <martinrtownley@gmail.com>
2025-05-05 22:17:39 +0100

    arrange layout

---
+++ src/content/textContent.js
@@ -0,0 +1,61 @@
+export const heroText0 = {
+  heading: "Psychotherapy and Counselling",
+  subheading: "Waltham Forest, London • Online",
+  button: "Get in touch",
+};
+
+export const heroText = {
+  heading: "Therapy in North East London",
+  subheading:
+    "Explore your thoughts and feelings in a warm and supportive space.",
+  button: "Get in touch",
+};
+
+export const introText = {
+  heading: "A warm and supportive space to explore your thoughts and feelings",
+  para1:
+    "Starting therapy can feel overwhelming, but you don’t have to do it alone. Whether you’re facing a specific challenge or just feeling stuck, therapy offers a space to reflect, gain clarity, and move forward at your own pace.",
+  para2:
+    "I offer a warm and supportive space where you can feel heard, understood, and empowered",
+};
+
+export const aboutText = {
+  heading: "About Me",
+  paras: [
+    "I am a psychotherapist in advanced clinical training, living and working in London. I have worked with clients both online and in person, developing my skills and practice working with a diverse range of clients, including those dealing with grief, anxiety, stress, trauma, parenthood and more.",
+
+    "For over ten years, I have also worked in the charity sector and have broad experience in the mental health and international development sectors.",
+
+    "I am a registered member of both UKATA and the UKCP, and follow their code of ethics. I  attend regular supervision and engage in continual professional development.",
+  ],
+};
+
+export const therapyText = {
+  heading: "Therapy",
+  paras: [
+    "Starting therapy can feel overwhelming, but you don’t have to do it alone. Whether you’re facing a specific challenge or just feeling stuck, therapy offers a space to reflect, gain clarity, and move forward at your own pace.",
+
+    "I offer a warm and supportive space where you can feel heard, understood and empowered.",
+
+    "My approach to therapy is grounded in Relational Transactional Analysis, with the openness to adapt to individual client’s needs.",
+  ],
+};
+
+export const sessionsText = {
+  heading: "Sessions",
+  paras: [
+    "I offer weekly individual 50 minute sessions, at the same time and day each week. We can work together for as long as you would like.",
+
+    "Current fees are affordable at £50 per session.",
+
+    "We can meet online or in person in the Waltham Forest area of London.",
+
+    "Our initial session together will be an opportunity to share what has led you to therapy, decide on whether we are a good match and discuss any questions you may have.",
+  ],
+};
+
+export const contactText = {
+  heading: "Contact Details",
+  email: "Email: contact@therapyjz.com",
+  phone: "Phone: 123456789",
+};
