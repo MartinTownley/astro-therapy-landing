66fdc38620cf5c7098aa826b9366b390a27106de
Martin Townley <martinrtownley@gmail.com>
2025-10-29 12:22:10 +0000

    WIP: unfinished approach section

--- src/content/textContent.js
+++ src/content/textContent.js
@@ -5,7 +5,7 @@ export const heroText0 = {
 }
 
 export const heroText = {
-  heading: 'Psychotherapy and counselling in Walthamstow',
+  heading: 'Counselling and Psychotherapy in Walthamstow',
   subheading: 'Explore your thoughts and feelings in a warm, supportive space.',
   button: 'Get in touch',
 }
@@ -13,17 +13,30 @@ export const heroText = {
 // intro text not in use
 export const introText = {
   heading: 'Affordable therapy in London.',
-  para: 'Starting therapy can feel like a big step, but you don’t have to do it alone. Whether you’re facing a specific challenge or just feeling stuck, therapy offers a space to reflect, gain clarity, and move forward at your own pace.',
+  paras: [
+    'Starting therapy can feel like a big step, but you don’t have to do it alone. Whether you’re facing a specific challenge or just feeling stuck, therapy offers a space to reflect, gain clarity, and move forward at your own pace.',
+
+    'I’m currently offering therapy at a reduced rate (£40 per session) whilst I complete my clinical training.',
+  ],
+}
+
+export const approachText = {
+  heading: 'My Approach',
+  paras: [
+    'I provide a confidential, inclusive, and non-judgmental space where you can feel heard, understood, and empowered.',
+
+    'My approach is grounded in Relational Transactional Analysis, which places the client-therapist relationship at the heart of the therapeutic process. At the same time, I remain flexible and responsive to each person’s unique needs and experiences.',
+  ],
 }
 
 export const aboutText = {
   heading: 'About Me',
   paras: [
-    'I have completed four years of advanced clinical training in counselling and psychotherapy, living and working in north east London. I’ve worked with clients both online and in person, developing my practice with a diverse range of people navigating experiences such as grief, anxiety, stress, trauma, parenthood, and more.',
+    'I have completed four years of advanced clinical training in counselling and psychotherapy, living and working in north east London. I’ve worked with clients both online and in person, developing my practice with a diverse range of people navigating experiences such as grief, anxiety, stress, trauma, parenthood and more.',
 
     'Alongside my clinical work, I have over ten years’ experience in the charity sector, with a broad background in mental health and international development.',
 
-    'I am a registered member of both the United Kingdom Council for Psychotherapy (UKCP) and the United Kingdom Association for Transactional Analysis (UKATA), and follow their code of ethics. I attend regular supervision and engage in continual professional development.',
+    'I am a registered member of both the United Kingdom Council for Psychotherapy (UKCP) and the United Kingdom Association for Transactional Analysis (UKATA) and follow their code of ethics. I attend regular supervision and engage in continual professional development.',
   ],
 }
 
@@ -51,6 +64,8 @@ export const sessionsText = {
 `,
   ],
 }
+
+export const whatToExpectText = {}
 
 export const contactText = {
   heading: 'Contact',
